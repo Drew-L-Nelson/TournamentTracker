049a368591d8de32e4a19d7c7905adfd5b95e575
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-14 15:42:58 -0700

    statements added

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,18 @@ namespace TrackerLibrary
     public static class GlobalConfig
     {
         public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database == true)
+            {
+                // TODO - Create the SQL Connection
+            }
+
+            if (textFiles == true)
+            {
+                // TODO - Create the Text Connection
+            }
+        }
     }
 }
