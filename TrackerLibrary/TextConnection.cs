28a6f44d03486b4d378d0744498edb00742fb946
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-21 12:48:25 -0700

    TrackerLibrary references added and InitializeConnections added to program dot cs

--- TrackerLibrary/TextConnection.cs
+++ TrackerLibrary/TextConnection.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    // TODO - Wire up the CreatePrize for text files
     public class TextConnection : IDataConnection
     {
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            throw new NotImplementedException();
+            model.Id = 1;
+
+            return model;
         }
     }
 }
