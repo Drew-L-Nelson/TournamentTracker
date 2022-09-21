a25fdf89866faa4c93180116b95ef5dc43768a15
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-21 12:27:37 -0700

    TextConnection created

--- TrackerLibrary/SqlConnector.cs
+++ TrackerLibrary/SqlConnector.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    // TODO - Make the CreatePrize method actually save to the database
+    /// <summary>
+    /// Saves a new prize to the database
+    /// </summary>
+    /// <param name="model">The prize information.</param>
+    /// <returns>The prize information, including the unique identifier.</returns>
     public class SqlConnector : IDataConnection
     {
         public PrizeModel CreatePrize(PrizeModel model)
