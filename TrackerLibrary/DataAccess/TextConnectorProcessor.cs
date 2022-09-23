5eca9cdcd4e1f693fa04ee597815d5ccc881e30b
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-23 15:36:48 -0700

    ConvertToPrizeModel

--- TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
 namespace TrackerLibrary.DataAccess.TextConnector
 {
     public static class TextConnectorProcessor
     {
-        public static string FullFilePath(string fileName) // PrizeModels.csv
+        public static string FullFilePath(this string fileName) // PrizeModels.csv
         {
-            return $"{ ConfigurationManager.AppSettings["filePath"] }";
+            // C:\Data\TournamentTracker\PrizeModels.csv
+            return $"{ ConfigurationManager.AppSettings["filePath"] }\\{ fileName } ";
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(',');
+            }
         }
     }
 }
