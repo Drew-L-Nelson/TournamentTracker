107bbd630606907407cf2e0191054889fe774051
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-26 15:27:02 -0700

    ready to test

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -29,7 +29,7 @@ namespace TrackerLibrary.DataAccess
             // Save the list<string> to the text file
             prizes.SaveToPrizeFile(PrizesFile);
 
-
+            return model;
         }
     }
 }
