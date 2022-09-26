101ad47174077900ed8aae3fdfd2c9d202344672
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-26 15:03:07 -0700

    Find the max Id, add the new record with the new Id

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -18,10 +18,15 @@ namespace TrackerLibrary.DataAccess
             // Load the text file and convert the text to List<PrizeModel>
             List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
 
-
-            
             // Find the max ID
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            model.Id = currentId;
+
             // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+            
+            
+            
             // Convert the prizes to a list<string>
             // Save the list<string> to the text file
 
