4be3200a02bfa2b91c73618df3706953448f5c4c
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-09-29 14:07:11 -0700

    remove button bug fixed

--- TrackerUI/CreateTeamForm.cs
+++ TrackerUI/CreateTeamForm.cs
@@ -105,20 +105,34 @@ namespace TrackerUI
         {
             PersonModel p = (PersonModel)selectTeamMemberDropDown.SelectedItem;
 
-            availableTeamMembers.Remove(p);
-            selectedTeamMembers.Add(p);
+            if (p != null)
+            {
+                availableTeamMembers.Remove(p);
+                selectedTeamMembers.Add(p);
 
-            WireUpLists();
+                WireUpLists(); 
+            }
+            else
+            {
+                MessageBox.Show("Please select a team memeber to add.");
+            }
         }
 
         private void removeSelectedMemeberButton_Click(object sender, EventArgs e)
         {
             PersonModel p = (PersonModel)teamMembersListBox.SelectedItem;
 
-            selectedTeamMembers.Remove(p);
-            availableTeamMembers.Add(p);
+            if (p != null)
+            {
+                selectedTeamMembers.Remove(p);
+                availableTeamMembers.Add(p);
 
-            WireUpLists();
+                WireUpLists(); 
+            }
+            else
+            {
+                MessageBox.Show("Please select a team memeber to remove.");
+            }
         }
     }
 }
