4f84e37c007e3645b688fa4b12f1e68fbafb931d
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-10-06 07:50:57 -0700

    getting back to right branch

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -7,22 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary.Models;
 
 namespace TrackerUI
 {
     public partial class CreateTournamentForm : Form
     {
+        List<TeamModel> availableTeams = new List<TeamModel>();
         public CreateTournamentForm()
         {
             InitializeComponent();
         }
 
         private void CreateTournamentForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
         {
 
         }
