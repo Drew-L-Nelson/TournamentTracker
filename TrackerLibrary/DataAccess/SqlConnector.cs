66ea7d76598141853016afe408f5bf2cb14d0986
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-10-06 11:25:22 -0700

    called InitializeLists() in CreateTournamentForm() method, fixed the bug

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -40,13 +40,6 @@ namespace TrackerLibrary.DataAccess
                 return model;
             }
         }
-
-        // TODO - Make the CreatePrize method actually save to the database
-        /// <summary>
-        /// Saves a new prize to the database
-        /// </summary>
-        /// <param name="model">The prize information.</param>
-        /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString(db)))
--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -19,6 +19,8 @@ namespace TrackerUI
         public CreateTournamentForm()
         {
             InitializeComponent();
+
+            InitializeLists();
         }
 
         private void InitializeLists()
