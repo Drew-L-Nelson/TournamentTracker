83876c41d9f8d89e12fe4564ed4b2e400a55564b
Drew Nelson <andrewnelson@shoemakermfg.com>
2022-11-04 11:37:24 -0700

    ConvertToMatchupEntryModel created

--- TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -193,6 +193,26 @@ namespace TrackerLibrary.DataAccess.TextHelpers
             }
         }
 
+        private static List<MatchupEntryModel> ConvertToMatchupEntryModel(this List<string> input)
+        {
+            //List<PersonModel> output = new List<PersonModel>();
+
+            //foreach (string line in lines)
+            //{
+            //    string[] cols = line.Split(',');
+
+            //    PersonModel p = new PersonModel();
+            //    p.Id = int.Parse(cols[0]);
+            //    p.FirstName = cols[1];
+            //    p.LastName = cols[2];
+            //    p.EmailAddress = cols[3];
+            //    p.CellphoneNumber = cols[4];
+            //    output.Add(p);
+            //}
+
+            //return output;
+        }
+
         private static List<MatchupEntryModel> ConvertStringToMatchupEntryModels(string input)
         {
             string[] ids = input.Split('|');
